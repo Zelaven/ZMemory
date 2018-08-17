b0ad4cb3cc627eea5613f68f2d95ac9d313b467a
Patrick Jakobsen <lollipop.zandronum@gmail.com>
2018-08-17 12:14:05 +0200

    Fixed acs folder retainer ending up in build The file is now properly excluded from building in both the standard and the thin build process

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ cd ..
 # Bundle everything that isn't:
 #  .sh  : a shell script.
 thin_arg=$1
-if [[ $thin_arg == "source" ]]; then
-  7za a -tzip ../ZMemory.pk3 -r -x\!*.sh -xr\!acs_source
+if [[ $thin_arg == "thin" ]]; then
+  7za a -tzip ../ZMemory.pk3 -r -x\!*.sh -x\!acs/.gitignore -xr\!acs_source
 else
-  7za a -tzip ../ZMemory.pk3 -r -x\!*.sh
+  7za a -tzip ../ZMemory.pk3 -r -x\!*.sh -x\!acs/.gitignore
 fi
 
 
